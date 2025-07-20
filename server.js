bc43b5249ff5c5273e80cf80c4a4ab5682c86462
Shubham Kasture <shubhamkasture289@gmail.com>
2025-07-20 22:48:57 +0530

    Api change

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post("/chat", async (req, res) => {
 
   try {
     const geminiRes = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyB4td9xo1MVfzgF50RB4enTXkvGNHsrB5s`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=API_KEY`,
       {
         method: "POST",
         headers: {
